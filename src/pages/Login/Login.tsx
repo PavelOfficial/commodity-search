65719c1b4966f8343fa520bc5702dfb25a9e3357
PavelOfficial <pavelofficial@ya.ru>
2026-02-08 21:05:48 +0300

    login form make up draft

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,28 +1,28 @@
 import "./Login.scss"
 import LoginSign from "./login-sign.svg?react"
-import { LoginForm } from "./LoginForm";
+import { LoginForm } from "./LoginForm/LoginForm";
 
 export const Login = () => {
   return (
-    <div class="auth-backdrop">
-      <div class="auth-card">
-        <div class="auth-card__inner">
-          <div class="auth-card__header">
-            <div class="auth-circle">
-              <div class="auth-circle__inner">
+    <div className="auth-backdrop">
+      <div className="auth-card">
+        <div className="auth-card__inner">
+          <div className="auth-card__header">
+            <div className="auth-circle">
+              <div className="auth-circle__inner">
                 <LoginSign />
               </div>
             </div>
           </div>
-          <h1 class="welcome-header">Добро пожаловать!</h1>
-          <div class="auth__call-to-action">Пожалуйста, авторизуйтесь</div>
+          <h1 className="welcome-header">Добро пожаловать!</h1>
+          <div className="auth__call-to-action">Пожалуйста, авторизуйтесь</div>
           <LoginForm />
-          <div class="hr-line">
-            <div class="hr-line__before-hr"></div>
-            <div class="hr-line__text">или</div>
-            <div class="hr-line__after-hr"></div>
+          <div className="hr-line">
+            <div className="hr-line__before-hr"></div>
+            <div className="hr-line__text">или</div>
+            <div className="hr-line__after-hr"></div>
           </div>
-          <div class="additional-actions">Нет аккаунта? <a href="#">Создать</a></div>
+          <div className="additional-actions">Нет аккаунта? <a href="#">Создать</a></div>
         </div>
       </div>
     </div>
---
+++ src/pages/Login/LoginForm/LoginForm.tsx
@@ -0,0 +1,36 @@
+import "./LoginForm.scss"
+import Cross from "./cross.svg?react"
+import UserIcon from "./user-icon.svg?react"
+import LockIcon from "./lock-icon.svg?react"
+import EyeInvisibleIcon from "./eye-invisible-icon.svg?react"
+import EyeVisibleIcon from "./eye-visible-icon.svg?react"
+
+export const LoginForm = () => {
+  return (
+    <form className="login-form">
+      <label className="form-label" htmlFor="user-name">Логин</label>
+      <div className="enhanced-input">
+        <div className="enhanced-input__icon">
+          <UserIcon />
+        </div>
+        <input name="user" id="user-name" />
+        <button className="enhanced-input__action">
+          <Cross />
+        </button>
+      </div>
+      <label className="form-label" htmlFor="password">Пароль</label>
+      <div className="enhanced-input">
+        <div className="enhanced-input__icon">
+          <LockIcon />
+        </div>
+        <input type="password" name="password" id="password" />
+        <button className="enhanced-input__action">
+          <EyeVisibleIcon />
+          <EyeInvisibleIcon />
+        </button>
+      </div>
+      <label className="form-label"><input type="checkbox" name="remember-me" /> Запомнить меня</label>
+      <button type="submit">Войти</button>
+    </form>
+  );
+}
