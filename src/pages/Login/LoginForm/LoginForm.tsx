f35b8ac81a040d4512c2916a4ff8c60259def66b
PavelOfficial <pavelofficial@ya.ru>
2026-02-09 11:55:33 +0300

    yap form validation

--- src/pages/Login/LoginForm/LoginForm.tsx
+++ src/pages/Login/LoginForm/LoginForm.tsx
@@ -1,5 +1,8 @@
-import { useForm   } from 'react-hook-form'
+import { useForm } from "react-hook-form"
 import { useState } from "react"
+
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 
 import "./LoginForm.scss"
 import Cross from "./cross.svg?react"
@@ -7,6 +10,13 @@ import UserIcon from "./user-icon.svg?react"
 import LockIcon from "./lock-icon.svg?react"
 import EyeInvisibleIcon from "./eye-invisible-icon.svg?react"
 import EyeVisibleIcon from "./eye-visible-icon.svg?react"
+
+// 1. Define the Yup validation schema
+const schema = yup.object({
+  userName: yup.string().required("User name is required").min(3, "User name must be at least 3 characters"),
+  password: yup.string().required("Password is required").min(4, "Password must be at least 4 characters"),
+  rememberMe: yup.boolean(),
+}).required();
 
 export const LoginForm = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -22,10 +32,10 @@ export const LoginForm = () => {
       password: "",
       rememberMe: false,
     },
+    resolver: yupResolver(schema)
   });
 
   const userNameValue = watch("userName");
-
 
   return (
     <form onSubmit={handleSubmit((data) => console.log(data))} className="login-form">
@@ -39,7 +49,7 @@ export const LoginForm = () => {
           <Cross />
         </button>}
       </div>
-      {errors.userName && <div>Username error</div>}
+      {errors.userName && <div className="form-error-message">{errors.userName.message}</div>}
       <label className="form-label" htmlFor="password">Пароль</label>
       <div className="enhanced-input">
         <div className="enhanced-input__icon">
@@ -50,11 +60,11 @@ export const LoginForm = () => {
           {passwordVisible ? <EyeVisibleIcon /> : <EyeInvisibleIcon />}
         </button>
       </div>
-      {errors.password && <div>Password error</div>}
+      {errors.password && <div className="form-error-message">{errors.password.message}</div>}
       <label className="form-label form-checkbox-label">
         <input className="form-checkbox-label__input" type="checkbox" {...register("rememberMe")} /><span className="form-checkbox-label__hint">Запомнить меня</span>
       </label>
-      <button class="primary-button primary-button_full-width form-submit" type="submit">Войти</button>
+      <button className="primary-button primary-button_full-width form-submit" type="submit">Войти</button>
     </form>
   );
 }
