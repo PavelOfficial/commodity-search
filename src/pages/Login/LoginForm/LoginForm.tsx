6838f1286819b35e6ab1ebefa6befa8fc03b8990
PavelOfficial <pavelofficial@ya.ru>
2026-02-09 11:41:17 +0300

    react hook form for login form

--- src/pages/Login/LoginForm/LoginForm.tsx
+++ src/pages/Login/LoginForm/LoginForm.tsx
@@ -1,3 +1,6 @@
+import { useForm   } from 'react-hook-form'
+import { useState } from "react"
+
 import "./LoginForm.scss"
 import Cross from "./cross.svg?react"
 import UserIcon from "./user-icon.svg?react"
@@ -6,31 +9,50 @@ import EyeInvisibleIcon from "./eye-invisible-icon.svg?react"
 import EyeVisibleIcon from "./eye-visible-icon.svg?react"
 
 export const LoginForm = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const {   
+    setValue,
+    register,
+    watch,
+    handleSubmit,    
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      userName: "",
+      password: "",
+      rememberMe: false,
+    },
+  });
+
+  const userNameValue = watch("userName");
+
+
   return (
-    <form className="login-form">
+    <form onSubmit={handleSubmit((data) => console.log(data))} className="login-form">
       <label className="form-label" htmlFor="user-name">Логин</label>
       <div className="enhanced-input">
         <div className="enhanced-input__icon">
           <UserIcon />
         </div>
-        <input name="user" id="user-name" />
-        <button className="enhanced-input__action">
+        <input {...register("userName")} id="user-name" />
+        {!!userNameValue && <button className="enhanced-input__action" onClick={() => setValue("userName", "")}>
           <Cross />
-        </button>
+        </button>}
       </div>
+      {errors.userName && <div>Username error</div>}
       <label className="form-label" htmlFor="password">Пароль</label>
       <div className="enhanced-input">
         <div className="enhanced-input__icon">
           <LockIcon />
         </div>
-        <input type="password" name="password" id="password" />
-        <button className="enhanced-input__action">
-          <EyeVisibleIcon />
-          <EyeInvisibleIcon />
+        <input type={passwordVisible ? "text" : "password"} {...register("password")}  id="password" />
+        <button className="enhanced-input__action" onClick={() => setPasswordVisible((visible) => !visible)}>
+          {passwordVisible ? <EyeVisibleIcon /> : <EyeInvisibleIcon />}
         </button>
       </div>
+      {errors.password && <div>Password error</div>}
       <label className="form-label form-checkbox-label">
-        <input className="form-checkbox-label__input" type="checkbox" name="remember-me" /><span className="form-checkbox-label__hint">Запомнить меня</span>
+        <input className="form-checkbox-label__input" type="checkbox" {...register("rememberMe")} /><span className="form-checkbox-label__hint">Запомнить меня</span>
       </label>
       <button class="primary-button primary-button_full-width form-submit" type="submit">Войти</button>
     </form>
