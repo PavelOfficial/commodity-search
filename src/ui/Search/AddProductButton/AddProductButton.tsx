56ed104f8f835528c5f97ea9ad354542e31a3557
PavelOfficial <pavelofficial@ya.ru>
2026-02-11 19:41:55 +0300

    add product form

--- src/ui/Search/AddProductButton/AddProductButton.tsx
+++ src/ui/Search/AddProductButton/AddProductButton.tsx
@@ -1,22 +1,49 @@
 import { useState } from 'react';
 import { Modal } from 'react-responsive-modal';
+import * as yup from "yup"
 
 import "./AddProductButton.scss"
 
 import Cross from "../../Login/LoginForm/cross.svg?react"
 
 import PlusCircle from "./plus-circle.svg?react"
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const schema = yup.object({
+  title: yup.string().required("Введите наименование"),
+  price: yup.string().required("Введите цену"),
+  brand: yup.string().required("Введите вендора"),
+  sku: yup.string().required("Введите артикул"),
+}).required();
 
 export const AddProductButton = () => {
+    const {   
+        register,
+        handleSubmit,    
+        formState: { errors },
+        reset,
+      } = useForm({
+        defaultValues: {
+          title: "",
+          price: "",
+          brand: "",
+          sku: "",
+        },
+        resolver: yupResolver(schema)
+      });
+
     const [modalIsOpen, setIsOpen] = useState(false);
 
     function openModal() {
+        reset();
         setIsOpen(true);
     }
 
     function closeModal() {
         setIsOpen(false);
     }
+
 
     return (
         <>
@@ -37,17 +64,18 @@ export const AddProductButton = () => {
                     </div>
                     <h2>Добавить товар</h2>
                     
-                    <form onSubmit={() => {}}>
+                    <form onSubmit={handleSubmit((state) => { console.log(state); closeModal(); })}>
                         <div>
                             <label className="form-label" htmlFor="title-input">
                                 Наименование
                             </label>
                             <input
+                                {...register("title")}
                                 className="form-input"
                                 type="text" 
-                                name="title" 
                                 id="title-input"
                             />
+                            {errors.title && <div className="form-error-message">{errors.title.message}</div>}
                         </div>
                         
                         <label>
@@ -55,33 +83,36 @@ export const AddProductButton = () => {
                                 Цена
                             </label>
                             <input 
+                                {...register("price")}
                                 className="form-input"
-                                type="number" 
-                                name="price" 
+                                type="number"
                                 id="price-input"
                             />
+                            {errors.price && <div className="form-error-message">{errors.price.message}</div>}
                         </label>
                         <label>
                             <label className="form-label" htmlFor="brand-input">
                                 Вендор
                             </label>
                             <input 
+                                {...register("brand")}
                                 className="form-input"
-                                type="text" 
-                                name="brand"
+                                type="text"
                                 id="brand-input" 
                             />
+                            {errors.brand && <div className="form-error-message">{errors.brand.message}</div>}
                         </label>
                         <label>
                             <label className="form-label" htmlFor="sku-input">
                                 Артикул
                             </label>
                             <input 
+                                {...register("sku")}
                                 className="form-input"
-                                type="text" 
-                                name="sku" 
+                                type="text"
                                 id="sku-input" 
                             />
+                            {errors.sku && <div className="form-error-message">{errors.sku.message}</div>}
                         </label>
                         <div className="form-controls">
                             <button className="primary-button primary-button_simple" type="submit">
